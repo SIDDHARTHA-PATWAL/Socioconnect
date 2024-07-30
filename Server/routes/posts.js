a88d82838e9ee320cd8d0090a5e2c4bfe1fb1a74
Siddhartha <siddharthapatwal@gmail.com>
2024-07-30 22:24:20 +0530

    enabled working 'like' feature

--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -8,6 +8,6 @@ router.get("/",verifyToken,getFeedPosts);
 router.get("/:userId/posts",verifyToken,getUserPosts);
 
 // UPDATE
-router.post("/:id/like",verifyToken,likePost);
+router.patch("/:id/like",verifyToken,likePost);
 
 export default router;
