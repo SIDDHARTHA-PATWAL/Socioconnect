961bf2ace898220ed8ba7a3cc9c073c6804e7c13
Siddhartha <siddharthapatwal@gmail.com>
2024-08-24 14:19:46 +0530

    updated baseURL.js

--- client/src/baseURL.js
+++ client/src/baseURL.js
@@ -1,6 +1,6 @@
 
-const baseURL = "https://socioconnect-api.vercel.app/";
+// const baseURL = "https://socioconnect-api.vercel.app/";
 // const baseURL = "http://localhost:3001/";
-// const baseURL = "https://socioconnect-ncfw.onrender.com/";
+const baseURL = "https://socioconnect-ncfw.onrender.com/";
 
 export default baseURL;
